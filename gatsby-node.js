0fa32a03ca8e79db81c5fd7d5956f5d85bc0e322
Pjaijai <87293994+Pjaijai@users.noreply.github.com>
2022-02-02 12:13:17 +0800

    .

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+// exports.onCreatePage = async ({ page, actions }) => {
+//   const { createPage } = actions;
+//   if (page.path.match(/^\/(?!dummy).*/)) {
+//     page.matchPath = "/:id";
+//     createPage(page);
+//   } else if (page.path.match(/^\/dummy/)) {
+//     page.matchPath = "/dummy/:id";
+//     createPage(page);
+//   }
+// };
